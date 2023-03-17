3c70ad5e6c428973524cc60cbf1904e864c4dd9c
Abdulmajeed <e-a.ahmed@lean.sa>
2023-03-17 16:05:44 +0300

    - Set up pokemon favorites UI and finalize it with data layer - Change some localization - Delete unused util functions and resources

--- app/src/main/java/com/example/demoapp/ui/favorite/PokemonFavoritesViewModel.kt
+++ app/src/main/java/com/example/demoapp/ui/favorite/PokemonFavoritesViewModel.kt
@@ -5,10 +5,12 @@ import com.example.demoapp.di.IoDispatcher
 import com.example.demoapp.domain.pokemon.model.Pokemon
 import com.example.demoapp.domain.pokemon.repository.PokemonRepository
 import com.example.demoapp.ui.common.base.viewmodel.BaseViewModel
+import com.example.demoapp.ui.common.state.UiState
 import com.example.demoapp.util.toUiState
 import com.example.demoapp.util.uiStateFlowOf
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -22,13 +24,36 @@ class PokemonFavoritesViewModel @Inject constructor(
     private val _uiState = uiStateFlowOf<List<Pokemon>>()
     val uiState = _uiState.asStateFlow()
 
+    private val _uiEvent = Channel<PokemonFavoritesEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    private val _deletePokemonEventState = Channel<UiState<Pokemon>>()
+    val deletePokemonEventState = _deletePokemonEventState.receiveAsFlow()
+
     fun getFavoritePokemonList() = pokemonRepository.getFavoritePokemonList()
         .onEach { _uiState.value = it.toUiState() }
         .catch { _uiState.value = it.toUiState() }
         .flowOn(ioDispatcher)
         .launchIn(viewModelScope)
 
-    fun deleteFavorite(pokemon: Pokemon) = viewModelScope.launch(ioDispatcher) {
-        pokemonRepository.deleteFavoritePokemon(pokemon)
+    fun onLongClickPokemon(pokemon: Pokemon) {
+        flowOf(pokemon)
+            .onEach { pokemonRepository.deleteFavoritePokemon(it) }
+            .onEach { _deletePokemonEventState.send(it.toUiState()) }
+            .catch { _deletePokemonEventState.send(it.toUiState()) }
+            .flowOn(ioDispatcher)
+            .launchIn(viewModelScope)
+    }
+
+    fun onClickPokemon(pokemonId: String) {
+        PokemonFavoritesEvent.OpenPokemonDetailsView(pokemonId).emit()
+    }
+
+    private fun PokemonFavoritesEvent.emit() = viewModelScope.launch {
+        _uiEvent.send(this@emit)
+    }
+
+    sealed class PokemonFavoritesEvent {
+        data class OpenPokemonDetailsView(val id: String) : PokemonFavoritesEvent()
     }
 }
