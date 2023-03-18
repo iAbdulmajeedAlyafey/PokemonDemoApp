12397e998cf30c58e643470293947fe6b4b9f182
Abdulmajeed <e-a.ahmed@lean.sa>
2023-03-18 14:32:23 +0300

    - User CallSuper annotations for some functions - Delete un-needed functions

--- app/src/main/java/com/example/demoapp/ui/common/base/fragment/BaseVMFragment.kt
+++ app/src/main/java/com/example/demoapp/ui/common/base/fragment/BaseVMFragment.kt
@@ -2,6 +2,7 @@ package com.example.demoapp.ui.common.base.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.CallSuper
 import androidx.viewbinding.ViewBinding
 import com.example.demoapp.ui.common.base.viewmodel.BaseViewModel
 
@@ -11,6 +12,7 @@ abstract class BaseVMFragment<VB : ViewBinding> : BaseFragment<VB>() {
 
     protected abstract fun observeUi()
 
+    @CallSuper
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         observeUi()
