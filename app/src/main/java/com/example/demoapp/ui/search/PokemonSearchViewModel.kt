47f587a4b50fc72476400f1ebe9559211dd94e98
Abdulmajeed <e-a.ahmed@lean.sa>
2023-03-19 15:17:44 +0300

    - Change annotation MockRepository to FakeRepository and rename related classes - Use shared flow when getting fake favorite pokemon list to simulate real code and experience

--- app/src/main/java/com/example/demoapp/ui/search/PokemonSearchViewModel.kt
+++ app/src/main/java/com/example/demoapp/ui/search/PokemonSearchViewModel.kt
@@ -3,7 +3,6 @@ package com.example.demoapp.ui.search
 import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.viewModelScope
 import com.example.demoapp.di.IoDispatcher
-import com.example.demoapp.di.MockRepository
 import com.example.demoapp.domain.pokemon.model.Pokemon
 import com.example.demoapp.domain.pokemon.repository.PokemonRepository
 import com.example.demoapp.ui.common.base.viewmodel.BaseViewModel
